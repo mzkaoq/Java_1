cd56ad2e7db3fefcbd36b4d0872a8a9c8f0d0bcf
mzkaoq <mzkaoq333@gmail.com>
2021-04-15 23:34:16 +0200

    ...

--- java_zad2/src/com/company/Main.java
+++ java_zad2/src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String m="czesc mam na imie filip i lubie kolor czerwony";
+        int w;
+        w = m.length();
+       String[] parts= m.split(" ");
+       String[] tab1=new String[parts.length];
+       for(int i=0;i<parts.length;i++){
+           tab1[i]=parts[i];
+        }
+        for(int i=0;i<parts.length;i++){
+            System.out.print(tab1[i] + " ");
+        }
+        System.out.println("\n"+parts.length);
+        String newstring;
+        newstring = String.join(" | ",tab1);
+        System.out.println(newstring);
+        for(int i=0;i<newstring.length();i++){
+          if(i%2==0) System.out.print(newstring.charAt(i));
+          else System.out.print("*");
+        }
     }
 }
