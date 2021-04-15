6ac8a44a037f4b375d6f55e1b7a1aff6102a9054
mzkaoq <mzkaoq333@gmail.com>
2021-04-16 00:48:50 +0200

    Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
    Example:
    Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
    The returned format must be correct in order to complete this challenge.
    Don't forget the space after the closing parentheses!

---
+++ zad5/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] x =new int[]{1,2,3,4,5,6,7,8,9};
+        Kata.createPhoneNumber(x);
+    }
+}
